93f820a3342d6f486e3209fb96504198efd6a80b
AVISHKA200332 <it23615502@my.sliit.lk>
2026-08-21 21:54:35 +0530

    Fix Error

--- metro.config.js
+++ metro.config.js
@@ -4,5 +4,6 @@ const config = getDefaultConfig(__dirname);
 
 // Ensure Metro resolves .cjs files for Firebase JS SDK compatibility
 config.resolver.sourceExts.push('cjs');
+config.resolver.unstable_enablePackageExports = false;
 
 module.exports = config;
